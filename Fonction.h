e47e791794325d8a677370a34cd8ed1dd3a8ecd3
Jesuisnoozy <aouallirendy@gmail.com>
2025-04-06 22:09:36 +0200

    Update Fonction.h

--- Fonction.h
+++ Fonction.h
@@ -4,4 +4,5 @@
 #include <math.h>
 #include <time.h>
 #include <errno.h>
-#include <fcntl.h> // Installé notamment pour Créer un fichier s'il existe pas
+#include <fcntl.h> // Pour Créer des fichiers (Je sais que y'a pas que ça mais j'en vois surtout cette utilité) 
+#include <unistd.h> // Pour supprimer des fichiers (idem)
