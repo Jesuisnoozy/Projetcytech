259acc36667d6b72b68b42033b844cf88be8f531
Jesuisnoozy <aouallirendy@gmail.com>
2025-04-30 16:05:05 +0200

    Update constante.h

--- constante.h
+++ constante.h
@@ -3,19 +3,19 @@
 
 #DEFINE LARGUEUR_ECRAN 50
 
-#DEFINE SPECIAL  // 1 pour l'activer, 0 pour le désactiver
+#DEFINE SPECIAL 1 // 1 pour l'activer, 0 pour le désactiver
 
-#DEFINE UNIQUE // 1 pour l'activer, 0 pour le désactiver
+#DEFINE UNIQUE 1 // 1 pour l'activer, 0 pour le désactiver
+
+#DEFINE POISON 1 // tu as compris je vais pas le répéter 
+
+#DEFINE EQUIPETAILLE 3
+
+#DEFINE STAMINA 1 //
+
+#DEFINE COOLDOWN 1 // 
 
 #DEFINE 
-
-#DEFINE
-
-#DEFINE
-
-#DEFINE
-
-#DEFINE
 
 #DEFINE
 
