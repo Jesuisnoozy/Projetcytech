8de3da08ab2806770283ef3280afc8ea4fe2f4dd
Jesuisnoozy <aouallirendy@gmail.com>
2025-04-30 15:52:17 +0200

    Update constante.h

--- constante.h
+++ constante.h
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#DEFINE HAUTEUR_ECRAN 90
+
+#DEFINE LARGUEUR_ECRAN 50
+
+#DEFINE SPECIAL  // 1 pour l'activer, 0 pour le désactiver
+
+#DEFINE UNIQUE // 1 pour l'activer, 0 pour le désactiver
+
+#DEFINE 
+
+#DEFINE
+
+#DEFINE
+
+#DEFINE
+
+#DEFINE
+
+#DEFINE
+
+#DEFINE
+
+#DEFINE
+
 
