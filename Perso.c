e60e07a70bb8a07ae19b1e9c681d00c62b6f585b
MaelysLou <maelys250506@gmail.com>
2025-05-07 15:18:10 +0200

    Update Perso.c

--- Perso.c
+++ Perso.c
@@ -8,24 +8,145 @@ ot Characterbuilder(int num) {
 		if(a.name==NULL) {
 			exit(1);
 		}
-		strcpy(a.name, "Bob");
-		break;
-	case 1000:
-		a=Builder(5, 50, 150, 0, 100, 200);
+		strcpy(a.name, "Forgeron");
+		break;// très bonne défense
+	case 2:
+		a=Builder(75, 25, 120, 0, 100, 200);
 		a.name=malloc(sizeof(char)*5);
 		if(a.name==NULL) {
 			exit(1);
 		}
-		strcpy(a.name, "Test");
-		break;
-	case 1001:
-		a=Builder(5, 50, 150, 1, 101, 201);
-		a.name=malloc(sizeof(char)*11);
+		strcpy(a.name, " Oracle);
+		break;// très bonne agilité
+
+	case 3:
+		a=Builder(45, 50, 225, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
 		if(a.name==NULL) {
 			exit(1);
 		}
-		strcpy(a.name, "TestPoison");
+		strcpy(a.name, "Magicien ");
+		break;// haut taux de PV
+		
+        case 4:
+		a=Builder(2, 50, 240, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Rôdeur");
+		break;// Tout++ car trop d'aura enfaite
+	case 5:
+		a=Builder(80, 35, 180, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Nécromancien ");
+		break;// PV/agilité+
+
+	case 6:
+		a=Builder(30, 40, 200, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Clerc");
+		break;// PV++
+
+	case 7:
+		a=Builder(0, 50, 100, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Illusionniste");
+		break;//  défense/agilité++
+
+
+
+	case 8:
+		a=Builder(90, 10, 190, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Druide);
 		break;
+		
+	case 9:
+		a=Builder(5, 30, 135, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Invocateur ");
+		break;
+
+	case 10:
+		a=Builder(5, 45, 225, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Champion");
+		break;
+
+	case 11:
+		a=Builder(66, 37, 111, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Explorateur");
+		break;
+
+	case 12:
+		a=Builder(100, 39, 168, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Chronomancien");
+		break;
+
+	case 13:
+		a=Builder(15, 30, 175, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Assassin");
+		break;
+
+	case 14:
+		a=Builder(73, 18, 190, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Juge");
+		break;
+
+	case 15:
+		a=Builder(55, 34, 250, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Empoisonneur");
+		break;
+
+	case 16:
+		a=Builder(5, 0, 187, 0, 100, 200);
+		a.name=malloc(sizeof(char)*5);
+		if(a.name==NULL) {
+			exit(1);
+		}
+		strcpy(a.name, "Sentinelle");
+		break;
+
+		
 	case 1002:
 		a=Builder(5, 50, 150, 2, 102, 202);
 		a.name=malloc(sizeof(char)*14);
